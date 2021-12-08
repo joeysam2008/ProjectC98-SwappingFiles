eef980f147fe685b2de5098527a9a98ca0fb9ff9
joeysam2008 <73652973+joeysam2008@users.noreply.github.com>
2021-12-08 16:03:10 +0530

    Add files via upload

---
+++ ProjectC98/swappingFile.py
@@ -0,0 +1,18 @@
+def swapFileData():
+    file1 = input("Enter the name of first file: ")
+    file2 = input("Enter the name of second file: ")
+    
+    a = open(file1, 'r')
+    data_a = a.read()
+
+    b = open(file2, 'r')
+    data_b = b.read()
+
+    a = open(file1, 'w')
+    a.write(data_b)
+
+    b = open(file2, 'w')
+    b.write(data_a) 
+
+swapFileData()                          
+print("Your files have been swapped!")
